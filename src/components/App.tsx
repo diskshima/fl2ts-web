a2b191dc6d7014587c5ee57b20a179df7da4b3a5
Daisuke Shimamoto <diskshima@gmail.com>
2019-03-09 15:48:25 +0900

    Add 2nd editor

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,19 +7,44 @@ const initialCode = '(num) => num + 1';
 export class App extends React.Component {
   state = { code: initialCode };
 
+  convert = (codeFrom: string) => {
+    return codeFrom + '!!!!!!!!';
+  }
+
+  renderFrom = () =>
+    <Editor
+      value={this.state.code}
+      onValueChange={(code: string) => this.setState({ code })}
+      highlight={(code: string) =>
+        Prism.highlight(code, Prism.languages.javascript)
+      }
+      padding={10}
+      style={{
+        fontFamily: '"Fira code", "Fira Mono", monospace',
+        // fontSize: 12,
+      }}
+    />;
+
+  renderTo = () =>
+    <Editor
+      value={this.convert(this.state.code)}
+      onValueChange={null}
+      highlight={(code: string) =>
+        Prism.highlight(code, Prism.languages.javascript)
+      }
+      padding={10}
+      style={{
+        fontFamily: '"Fira code", "Fira Mono", monospace',
+        // fontSize: 12,
+      }}
+    />;
+
   render() {
     return (
-      <Editor
-        value={this.state.code}
-        onValueChange={(code: string) => this.setState({ code })}
-        highlight={(code: string) =>
-          Prism.highlight(code, Prism.languages.javascript)
-        }
-        padding={10}
-        style={{
-          fontFamily: '"Fira code", "Fira Mono", monospace',
-          // fontSize: 12,
-        }}
-      />);
+      <div className="app">
+        {this.renderFrom()}
+        {this.renderTo()}
+      </div>
+    );
   }
 }
