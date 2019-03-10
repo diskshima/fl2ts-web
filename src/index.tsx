b2d25107d8466764bee689990a022be3fd075621
Daisuke Shimamoto <diskshima@gmail.com>
2019-03-10 16:31:53 +0900

    Remove Hello component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { Hello } from './components/Hello';
 import { App } from './components/App';
 
 ReactDOM.render(
   <div>
-    <Hello compiler="TypeScript" framework="React" />
     <App />
   </div>,
   document.getElementById("app")
